09cf5855ef47a67774f06d12a21381a2f294d961
diptodev <dipankardebnathcse@yahoo.com>
2023-06-18 01:41:38 +0600

    library updated

--- app/mylibrary/src/main/java/com/excitedbroltd/mylibrary/MyProgressBar.kt
+++ app/mylibrary/src/main/java/com/excitedbroltd/mylibrary/MyProgressBar.kt
@@ -1,10 +1,16 @@
 package com.excitedbroltd.mylibrary
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -17,42 +23,53 @@ class MyProgressBar {
     companion object {
 
         @Composable
-        fun waveCanvas(
-            startAngleSecondary: Float = 150f,
-            sweepAngleSecondary: Float = 240f,
-            startAnglePrimary: Float = 150f,
-            sweepAnglePrimary: Float = 0f,
+          fun waveCanvas(
+            startAngle: Float = 150f,
+            sweepAngle: Float = 240f,
+            progressValue: Float = 0f,
             progressbarWidth: Int = 15,
+            maximumProgress: Float = 100f,
             strokeCap: StrokeCap = StrokeCap.Round,
             secondaryColor: Color = Color.LightGray.copy(alpha = 0.5f),
             primaryColor: Color = Color.Blue.copy(alpha = 0.8f),
             modifier: Modifier = Modifier
                 .padding(10.dp)
                 .size(300.dp),
-            useCenter: Boolean = false
-
+            useCenter: Boolean = false,
+            text: String = "0 GB",
+            textModifier: Modifier = Modifier.padding(10.dp),
+            animationDuration: Int = 1000
         ) {
+            var existingValue by remember {
+                mutableStateOf(0f)
+            }
+            if (progressValue <= maximumProgress) existingValue = progressValue
+            var progressValue = if (progressValue <= maximumProgress) progressValue else existingValue
+            val animate by animateFloatAsState(
+                targetValue = (progressValue / 100) * 240 * (100 / maximumProgress),
+                animationSpec = tween(animationDuration)
+            )
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = modifier
                     .drawBehind {
                         drawArc(
-                            startAngle = startAngleSecondary,
-                            sweepAngle = sweepAngleSecondary,
+                            startAngle = startAngle,
+                            sweepAngle = sweepAngle,
                             useCenter = useCenter,
                             color = secondaryColor,
                             style = Stroke(width = progressbarWidth.dp.toPx(), cap = strokeCap),
                         )
                         drawArc(
-                            startAngle = startAnglePrimary,
-                            sweepAngle = sweepAnglePrimary,
+                            startAngle = startAngle,
+                            sweepAngle = animate,
                             useCenter = useCenter,
                             color = primaryColor,
                             style = Stroke(width = progressbarWidth.dp.toPx(), cap = strokeCap),
                         )
 
                     }) {
-                Text(text = "10GB")
+                Text(text = text, modifier = textModifier)
             }
 
         }
--- app/src/main/java/com/excitedbroltd/jetpackcanvas/MainActivity.kt
+++ app/src/main/java/com/excitedbroltd/jetpackcanvas/MainActivity.kt
@@ -3,15 +3,10 @@ package com.excitedbroltd.jetpackcanvas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,14 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.excitedbroltd.jetpackcanvas.ui.theme.JetpackCanvasTheme
+import com.excitedbroltd.mylibrary.MyProgressBar
+import com.excitedbroltd.mylibrary.MyProgressBar.Companion.waveCanvas
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -57,8 +49,8 @@ class MainActivity : ComponentActivity() {
                     waveCanvas(
                         text = text,
                         progressValue = progressValue,
-                        maximumProgress = 80f,
-                        animationDuration = 500
+                        maximumProgress = 1000f,
+                        animationDuration = 1000
                     )
                     TextField(
                         value = "$progressValue",
@@ -81,60 +73,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     @Preview(showBackground = true)
     fun previewWaveCanvas() {
-        waveCanvas(text = "100 GB")
-    }
-
-
-    @Composable
-    private fun waveCanvas(
-        startAngle: Float = 150f,
-        sweepAngle: Float = 240f,
-        progressValue: Float = 0f,
-        progressbarWidth: Int = 15,
-        maximumProgress: Float = 100f,
-        strokeCap: StrokeCap = StrokeCap.Round,
-        secondaryColor: Color = Color.LightGray.copy(alpha = 0.5f),
-        primaryColor: Color = Color.Blue.copy(alpha = 0.8f),
-        modifier: Modifier = Modifier
-            .padding(10.dp)
-            .size(300.dp),
-        useCenter: Boolean = false,
-        text: String = "0 GB",
-        textModifier: Modifier = Modifier.padding(10.dp),
-        animationDuration: Int = 1000
-    ) {
-        var existingValue by remember {
-            mutableStateOf(0f)
-        }
-        if (progressValue < maximumProgress) existingValue = progressValue
-        var progressValue = if (progressValue < maximumProgress) progressValue else existingValue
-        val animate by animateFloatAsState(
-            targetValue = (progressValue / 100) * 240 * (100 / maximumProgress),
-            animationSpec = tween(animationDuration)
-        )
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-                .drawBehind {
-                    drawArc(
-                        startAngle = startAngle,
-                        sweepAngle = sweepAngle,
-                        useCenter = useCenter,
-                        color = secondaryColor,
-                        style = Stroke(width = progressbarWidth.dp.toPx(), cap = strokeCap),
-                    )
-                    drawArc(
-                        startAngle = startAngle,
-                        sweepAngle = animate,
-                        useCenter = useCenter,
-                        color = primaryColor,
-                        style = Stroke(width = progressbarWidth.dp.toPx(), cap = strokeCap),
-                    )
-
-                }) {
-            Text(text = text, modifier = textModifier)
-        }
-
+        waveCanvas()
     }
 
 }
