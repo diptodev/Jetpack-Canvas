05c3f33d490e77200a137a75bd683e1838dc099b
diptodev <dipankardebnathcse@yahoo.com>
2023-06-17 14:09:02 +0600

    remove basic template

--- app/src/main/java/com/excitedbroltd/jetpackcanvas/MainActivity.kt
+++ app/src/main/java/com/excitedbroltd/jetpackcanvas/MainActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.excitedbroltd.jetpackcanvas.ui.theme.JetpackCanvasTheme
 
@@ -20,27 +19,25 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Blue
                 ) {
-                    Greeting("Android")
+                    waveCanvas()
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackCanvasTheme {
-        Greeting("Android")
     }
+
+    @Composable
+    fun waveCanvas() {
+
+    }
+
+    @Composable
+    @Preview(showBackground = true, showSystemUi = true)
+    fun previewWaveCanvas() {
+        waveCanvas()
+    }
+
 }
